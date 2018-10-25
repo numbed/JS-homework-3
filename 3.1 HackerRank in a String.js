a426aeb4da8acf6dbc45c4f3cbeff11638b71266
Nikola Rashkov <rashkov.nikola@gmail.com>
2018-10-25 10:45:01 +0300

    Update 3.1 HackerRank in a String.js

--- 3.1 HackerRank in a String.js
+++ 3.1 HackerRank in a String.js
@@ -2,3 +2,25 @@
     https://www.hackerrank.com/challenges/hackerrank-in-a-string/
 */
 
+function run(){
+	
+	function hasString(s, ss){
+		var ssIndex = 0;
+		for (var i=0; i<s.length; i++){
+			if (s[i] == ss[ssIndex]){
+				console.log(s[i] + ss[ssIndex]);
+				ssIndex += 1;
+			}
+		}
+		return ssIndex == ss.length;
+	}
+		
+	var q = prompt("enter number of queries:",2);
+	for (var x=0; x<q; x++){
+		var s = prompt("Enter string:","haacckkerrannkk");
+		var output = hasString(s, "hackerrank");
+		console.log(output ? "yes" : "no");
+	} 
+			
+}
+run();
