948941e50a5fd4f5c6ec657b56b8fb02e4c9ab74
Nikola Rashkov <rashkov.nikola@gmail.com>
2018-10-27 15:52:06 +0300

    Update 3.3 Counting Valleys.js

--- 3.3 Counting Valleys.js
+++ 3.3 Counting Valleys.js
@@ -3,4 +3,26 @@
 */
 
 
+function main() {
 
+	var steps = parseInt(prompt("Enter Steps:", 8));
+	var path = prompt("Enter path ([U|D]):", "UDDDUDUUUDDDUDUUUDDDUDUU").toUpperCase();
+	console.log(path);
+	var elevation = 0;
+	var valey = 0;
+	for (var x in path) {
+		if (path[x] == "U") {
+			elevation += 1;
+		} else if (path[x] == "D") {
+			elevation -= 1;
+		}
+
+		if (elevation == 0) {
+			valey += 1;
+		}
+	}
+
+	console.log(valey / 2);
+
+}
+main();
