b222f281124ef14103d9deea6a5c518a86792085
Nikola Rashkov <rashkov.nikola@gmail.com>
2018-10-28 00:01:11 +0300

    Update 3.5 Cooking by Numbers.js

---
+++ 3.5 Cooking by Numbers.js
@@ -0,0 +1,23 @@
+/*
+
+Write a program that receives a number and a list of five operations.
+Perform the operations in sequence by starting with the input number
+and using the result of every operation as starting point for the next.
+Print the result of every operation in order.
+
+The operations can be one of the following:
+•	chop – divide the number by two
+•	dice – square root of number
+•	spice – add 1 to number
+•	bake – multiply number by 3
+•	fillet – subtract 20% from number
+The input comes in 2 lines.
+	On the first line you will receive your starting point
+	and must be parsed to a number.
+	On the second line you will receive 5 commands separated by “, “
+	each one will be the name of the operation to be performed.
+
+The output should be printed on the console.
+
+
+*/
