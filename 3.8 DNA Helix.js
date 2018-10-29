21f6b88f4b310442aa1f472f3e8d363daba05d97
Nikola Rashkov <rashkov.nikola@gmail.com>
2018-10-29 17:35:26 +0200

    Update 3.8 DNA Helix.js

---
+++ 3.8 DNA Helix.js
@@ -0,0 +1,18 @@
+/*
+
+Write a program that prints a DNA helix with length, specified by the user.
+The helix has a repeating structure, but the symbol in the chain follows the sequence ATCGTTAGGG.
+See the examples for more information.
+The input comes as a single string element that must be parsed to a number.
+It represents the length of the required helix.
+The output is the completed structure, printed on the console.
+
+*/
+
+function main(){
+
+
+
+
+}
+main();
