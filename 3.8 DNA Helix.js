3d05a16f0ba11d4d5aad827931e88acd3ecd0079
Nikola Rashkov <rashkov.nikola@gmail.com>
2018-10-30 09:39:05 +0200

    Update 3.8 DNA Helix.js

--- 3.8 DNA Helix.js
+++ 3.8 DNA Helix.js
@@ -7,11 +7,29 @@ The input comes as a single string element that must be parsed to a number.
 It represents the length of the required helix.
 The output is the completed structure, printed on the console.
 
+Input	Output		Input	Output
+    4	              **AT**
+                    *C--G*
+                    T----T
+                    *A--G*
+
+	10	                **AT**
+                      *C--G*
+                      T----T
+                      *A--G*
+                      **GG**
+                      *A--T*
+                      C----G
+                      *T--T*
+                      **AG**
+                      *G--G*
+
+
 */
 
 function main(){
 
-
+var helixSize = parseInt(prompt("Enter length of the helix:"));
 
 
 }
